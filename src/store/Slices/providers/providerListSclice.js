c7b11180857809a95c384f222832abf8c09fb812
Dustin10091984 <rodneytinneydustin@gmail.com>
2021-12-30 17:51:39 +0500

    add loading in states

--- src/store/Slices/providers/providerListSclice.js
+++ src/store/Slices/providers/providerListSclice.js
@@ -18,6 +18,7 @@ const { getProvider } = providerSlice.actions;
 
 export const getProviderList = (params) => async dispatch => {
     try {
+        getProvider({ error: false, loading: true });
         await axios({
             method: 'get',
             // headers: {
@@ -26,11 +27,16 @@ export const getProviderList = (params) => async dispatch => {
             url: process.env.REACT_APP_API_BASE_URL + `/api/user/services/provider-list${params}`,
         }).then((response) => {
             //handle success
+            let data = response.data;
+            data.loading = false;
             dispatch(getProvider(response.data));
         }).catch((error) => {
+            //handle error
+            let data = error.response.data;
+            data.loading = false;
             dispatch(getProvider(error.response.data));
         });
     } catch (error) {
-        dispatch(getProvider({error: true, message: "something went wrong!"}));
+        dispatch(getProvider({ error: true, loading: false, message: "something went wrong!" }));
     }
 }
