2c47acac8b7dffb325c6bd550ae495258a82ea1e
Dustin10091984 <rodneytinneydustin@gmail.com>
2021-12-30 18:00:44 +0500

    register sw.js file

---
+++ src/sw-register.js
@@ -0,0 +1,10 @@
+export default function LocalServiceWorkerRegister() {
+    const swPath = `http://localhost:3000/firebase-messaging-sw.js`;
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register(swPath).then(registration => {
+                console.log('Service worker registered', registration);
+            });
+        });
+    }
+}
