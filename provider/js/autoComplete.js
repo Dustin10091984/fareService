669278b6e09712ee57bb55f6e29e7983e0dee16e
Dustin10091984 <rodneytinneydustin@gmail.com>
2022-01-26 10:47:58 +0500

    add auto complete address

---
+++ provider/js/autoComplete.js
@@ -0,0 +1,21 @@
+$(window).ready(function () {
+    if (google && google.maps.places) {
+        var autocomplete = new google.maps.places.Autocomplete($("#zipCode")[0], {
+        });
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var place = autocomplete.getPlace();
+            var zipcode = ''
+            for (var j = 0; j < place.address_components.length; j++) {
+                for (var k = 0; k < place.address_components[j].types.length; k++) {
+                    if (place.address_components[j].types[k] == "postal_code") {
+                        zipcode = place.address_components[j].short_name;
+                    }
+                }
+            }
+            if (zipcode != '') {
+                $("#hiddenZipCode").val(zipcode);
+                console.log($("#hiddenZipCode").val());
+            }
+        });
+    }
+});
