e8c57b466954b164706e4a90ecf4f865c5a34a7a
Dustin10091984 <rodneytinneydustin@gmail.com>
2022-04-25 17:35:03 +0500

    page issue

--- src/front-end/Page.jsx
+++ src/front-end/Page.jsx
@@ -9,22 +9,21 @@ export const Page = (props) => {
     const { match: { params: { name } } } = props;
 
     const pages = useSelector((state) => state?.footerReducer?.pages);
-    
+    const page = pages?.data?.find((page) => (page.name, name));
+
     useEffect(() => {
-        if(name && pages) {
-            const page = pages?.data?.find((page) => (page.name == name));
-            setState((state)=>({...state, title: page?.title}));
-            const converter = new QuillDeltaToHtmlConverter(JSON.parse(page?.content)?.ops);
-            setState((state) => ({...state, content: converter.convert()}));
+        if(page?.content) {
+            var converter = new QuillDeltaToHtmlConverter(JSON.parse(page?.content)?.ops);
+            setState(converter.convert());
         }
-    }, [name]);
-    
+    }, [page?.content]);
+
     return (
         <div className="container">
             <Loading loading={pages.loading}></Loading>
             <div className="row">
                 <div className="text-center col-md-12">
-                    <h1>{state?.title}</h1>
+                    <h1>{page?.name}</h1>
                 </div>
                 <div className="col-md-12 order-box-des d-flex  align-items-center">
                 <p
@@ -32,7 +31,7 @@ export const Page = (props) => {
                         fontSize: '1.5rem',
                     }}
                     className="Features"
-                    dangerouslySetInnerHTML={{ __html: state?.content }}
+                    dangerouslySetInnerHTML={{ __html: state }}
                 />
                     {pages.error && (
                         <div className="order-num">Not Found Data</div>
