b98cd2a8191becd7c41c41a1b658141a2e57c451
Dustin10091984 <rodneytinneydustin@gmail.com>
2022-04-25 20:19:57 +0500

    page issue

--- src/front-end/Page.jsx
+++ src/front-end/Page.jsx
@@ -4,27 +4,26 @@ import Loading from './common/Loading';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 
 export const Page = (props) => {
-    const [state, setState] = useState({});
+    const [state, setState] = useState();
 
     const { match: { params: { name } } } = props;
 
     const pages = useSelector((state) => state?.footerReducer?.pages);
-    
+    const page = pages?.data?.find((page) => (page.name, name));
+
     useEffect(() => {
         if(page?.content) {
-            const page = pages?.data?.find((page) => (page.name == name));
-            const converter = new QuillDeltaToHtmlConverter(JSON.parse(page?.content)?.ops);
-            converter = converter.convert();
-            // setState((prev) => ({...prev, content: converter, title: page?.title}));
+            var converter = new QuillDeltaToHtmlConverter(JSON.parse(page?.content)?.ops);
+            setState((state)=>(converter.convert()));
         }
     }, [page?.content]);
-    
+
     return (
         <div className="container">
             <Loading loading={pages.loading}></Loading>
             <div className="row">
                 <div className="text-center col-md-12">
-                    <h1>{state?.title}</h1>
+                    <h1>{page?.name}</h1>
                 </div>
                 <div className="col-md-12 order-box-des d-flex  align-items-center">
                 <p
@@ -32,7 +31,7 @@ export const Page = (props) => {
                         fontSize: '1.5rem',
                     }}
                     className="Features"
-                    dangerouslySetInnerHTML={{ __html: state?.content }}
+                    dangerouslySetInnerHTML={{ __html: state }}
                 />
                     {pages.error && (
                         <div className="order-num">Not Found Data</div>
