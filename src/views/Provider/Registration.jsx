fdd402b5e03f920b35c5b5bcef7a0b023675bf5c
Dustin10091984 <rodneytinneydustin@gmail.com>
2022-05-12 17:31:16 +0500

    services issue

--- src/views/Provider/Registration.jsx
+++ src/views/Provider/Registration.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-import Registration from "./../../front-end/ProviderRegistration";
+import Registration from "./../../front-end/ProviderRegistration/index.js";
 
 const RegistrationPage = (props) => {
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
         document.title = "Farenow: Provider Registration From";
     });
-    return <Registration />;
+    return <Registration {...props} />;
 };
 
 export { RegistrationPage };
