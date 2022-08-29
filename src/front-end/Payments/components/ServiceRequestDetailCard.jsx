1ac6a73791348fea624c92b969d4ef41ab1a2416
Dustin10091984 <rodneytinneydustin@gmail.com>
2022-08-29 15:30:22 +0500

    show service request

--- src/front-end/Payments/components/ServiceRequestDetailCard.jsx
+++ src/front-end/Payments/components/ServiceRequestDetailCard.jsx
@@ -1,16 +1,26 @@
-const ServiceRequestDetailCard = ({ state, serviceDetail }) => {
+import { memo } from "react";
+
+const ServiceRequestDetailCard = memo(({ state, serviceDetail }) => {
     return (
         <div className="cart-total d-flex align-items-center justify-content-between">
-            <div className="cart-title">
-                {state.type ? "Product Name" : "Service Request"}
+            <div className="cart-title pt-3">
+                <div className="mt-2">{"Service Name: Cleaning"}</div>
+                <div className="mt-2">
+                    Provider Name: {serviceDetail?.provider?.first_name}
+                </div>
+                <address className="mt-2">
+                    Address: {serviceDetail?.address}
+                </address>
+                <small>Slot: 00:10</small>
+                <br />
+                <small>Date: {serviceDetail?.dateTime}</small>
             </div>
             <div className="price-qnt-subtotal">
                 <ul className="list-heading d-flex align-items-center justify-content-between w-100">
-                    <li>{state.type ? "Price" : "Hourly Rate"}</li>
-                    <li>{state.type ? "Quantity" : "Total Hours"}</li>
+                    <li>{state?.type ? "Price" : "Hourly Rate"}</li>
+                    <li>{state?.type ? "Quantity" : "Total Hours"}</li>
                     <li>Total</li>
                 </ul>
-
                 <ul className="list-des d-flex align-items-center justify-content-between w-100">
                     <li>{`$${serviceDetail?.provider?.provider_profile?.hourly_rate}`}</li>
                     <li>{serviceDetail?.hours}</li>
@@ -22,6 +32,6 @@ const ServiceRequestDetailCard = ({ state, serviceDetail }) => {
             </div>
         </div>
     );
-};
+});
 
 export { ServiceRequestDetailCard };
